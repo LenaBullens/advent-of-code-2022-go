aec5f17f6ece02278ff9a8f884c79fbd43885de2
LenaBullens <BullensLena@gmail.com>
2022-12-28 02:21:00 +0100

    day 21 part 2

--- source/21-MonkeyMath/main.go
+++ source/21-MonkeyMath/main.go
@@ -22,6 +22,7 @@ type Monkey struct {
 	name        string
 	value       int
 	operation   Operation
+	parent      string
 	leftMonkey  string
 	rightMonkey string
 	monkeyMap   map[string]Monkey
@@ -31,12 +32,37 @@ func createMonkey(name string, value int, operation Operation, leftMonkey string
 	return Monkey{name: name, value: value, operation: operation, leftMonkey: leftMonkey, rightMonkey: rightMonkey, monkeyMap: monkeyMap}
 }
 
+func (monkey Monkey) getParent() Monkey {
+	return monkey.monkeyMap[monkey.parent]
+}
+
 func (monkey Monkey) getLeftMonkey() Monkey {
 	return monkey.monkeyMap[monkey.leftMonkey]
 }
 
 func (monkey Monkey) getRightMonkey() Monkey {
 	return monkey.monkeyMap[monkey.rightMonkey]
+}
+
+func (monkey Monkey) getOtherChild(child Monkey) Monkey {
+	if child.name == monkey.leftMonkey {
+		return monkey.getRightMonkey()
+	} else {
+		return monkey.getLeftMonkey()
+	}
+}
+
+func (monkey Monkey) fixParents() {
+	if monkey.leftMonkey != "" {
+		leftMonkey := monkey.monkeyMap[monkey.leftMonkey]
+		leftMonkey.parent = monkey.name
+		monkey.monkeyMap[monkey.leftMonkey] = leftMonkey
+	}
+	if monkey.rightMonkey != "" {
+		rightMonkey := monkey.monkeyMap[monkey.rightMonkey]
+		rightMonkey.parent = monkey.name
+		monkey.monkeyMap[monkey.rightMonkey] = rightMonkey
+	}
 }
 
 func (monkey Monkey) getValue() int {
@@ -61,8 +87,46 @@ func (monkey Monkey) getValue() int {
 	return 0
 }
 
+func (monkey Monkey) getRequiredValue() int {
+	// If we're already at the level below root, we just need to equate the value of the
+	// other branch of the tree.
+	if monkey.parent == "root" {
+		return monkey.getParent().getOtherChild(monkey).getValue()
+	} else {
+		// Else we go up one level, and 'revert' the operation of the parent.
+		if monkey.name == monkey.getParent().leftMonkey {
+			if monkey.getParent().operation == Add {
+				return monkey.getParent().getRequiredValue() - monkey.getParent().getOtherChild(monkey).getValue()
+			}
+			if monkey.getParent().operation == Subtract {
+				return monkey.getParent().getRequiredValue() + monkey.getParent().getOtherChild(monkey).getValue()
+			}
+			if monkey.getParent().operation == Multiply {
+				return monkey.getParent().getRequiredValue() / monkey.getParent().getOtherChild(monkey).getValue()
+			}
+			if monkey.getParent().operation == Divide {
+				return monkey.getParent().getRequiredValue() * monkey.getParent().getOtherChild(monkey).getValue()
+			}
+		} else {
+			if monkey.getParent().operation == Add {
+				return monkey.getParent().getRequiredValue() - monkey.getParent().getOtherChild(monkey).getValue()
+			}
+			if monkey.getParent().operation == Subtract {
+				return monkey.getParent().getOtherChild(monkey).getValue() - monkey.getParent().getRequiredValue()
+			}
+			if monkey.getParent().operation == Multiply {
+				return monkey.getParent().getRequiredValue() / monkey.getParent().getOtherChild(monkey).getValue()
+			}
+			if monkey.getParent().operation == Divide {
+				return monkey.getParent().getOtherChild(monkey).getValue() / monkey.getParent().getRequiredValue()
+			}
+		}
+	}
+	return 0
+}
+
 func main() {
-	solve1()
+	solve2()
 }
 
 func solve1() {
@@ -108,4 +172,53 @@ func solve1() {
 	}
 
 	fmt.Println(monkeyMap["root"].getValue())
+}
+
+func solve2() {
+	lines := helper.ReadLines("input-21.txt")
+	monkeyMap := make(map[string]Monkey)
+
+	for _, line := range lines {
+		splitLine := strings.Split(line, ": ")
+		name := splitLine[0]
+		var operation Operation
+		var value int
+		var leftMonkey string
+		var rightMonkey string
+		if strings.Contains(splitLine[1], "+") {
+			operation = Add
+			operationSplit := strings.Split(splitLine[1], " + ")
+			leftMonkey = operationSplit[0]
+			rightMonkey = operationSplit[1]
+		} else if strings.Contains(splitLine[1], "-") {
+			operation = Subtract
+			operationSplit := strings.Split(splitLine[1], " - ")
+			leftMonkey = operationSplit[0]
+			rightMonkey = operationSplit[1]
+		} else if strings.Contains(splitLine[1], "*") {
+			operation = Multiply
+			operationSplit := strings.Split(splitLine[1], " * ")
+			leftMonkey = operationSplit[0]
+			rightMonkey = operationSplit[1]
+		} else if strings.Contains(splitLine[1], "/") {
+			operation = Divide
+			operationSplit := strings.Split(splitLine[1], " / ")
+			leftMonkey = operationSplit[0]
+			rightMonkey = operationSplit[1]
+		} else {
+			value, _ = strconv.Atoi(splitLine[1])
+			operation = Invalid
+			leftMonkey = ""
+			rightMonkey = ""
+		}
+
+		monkey := createMonkey(name, value, operation, leftMonkey, rightMonkey, monkeyMap)
+		monkeyMap[name] = monkey
+	}
+
+	for _, v := range monkeyMap {
+		v.fixParents()
+	}
+
+	fmt.Println(monkeyMap["humn"].getRequiredValue())
 }
